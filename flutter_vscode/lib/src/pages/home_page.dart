4e4d3ea7ed86d08769779aa230769048d787a14a
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-01-06 16:46:21 -0600

    Botón flotante - FloatingActionButton

--- flutter_vscode/lib/src/pages/home_page.dart
+++ flutter_vscode/lib/src/pages/home_page.dart
@@ -9,25 +9,34 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        /* algo que cubre toda la pantalla, tenemos un navbar, etc */
-        appBar: AppBar(
-          title: Text('Título'),
-          centerTitle: true,
-        ),
-        body: Center(
-            child: Column(
-          /* para poner varios elemenetos se necesita en columna */
-          mainAxisAlignment: MainAxisAlignment.center,
-          /* esto lo centran con el mainAxisAligment puedes centrarlo de varias maneras */
-          children: <Widget>[
-            Text(
-              'Número de taps:',
-              style: estiloTexto,
-            ),
-            Text('0', style: estiloTexto),
-          ],
-        )
-            /* no se puede poner otro child abajo, solo 1 */
-            ));
+      /* algo que cubre toda la pantalla, tenemos un navbar, etc */
+      appBar: AppBar(
+        title: Text('Título'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+        /* para poner varios elemenetos se necesita en columna */
+        mainAxisAlignment: MainAxisAlignment.center,
+        /* esto lo centran con el mainAxisAligment puedes centrarlo de varias maneras */
+        children: <Widget>[
+          Text(
+            'Número de taps:',
+            style: estiloTexto,
+          ),
+          Text('0', style: estiloTexto),
+        ],
+      )
+          /* no se puede poner otro child abajo, solo 1 */
+          ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        /* para que aparezca un icono, este puede ser cualquier cosa también */
+        onPressed: () {
+          /* una funcion recibe, es el boton para presionar */
+          print('Hola mundo');
+        },
+      ),
+    );
   }
 }
