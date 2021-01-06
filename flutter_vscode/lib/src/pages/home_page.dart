d4d323f3085f39059cfaaea521996d067199af2c
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-01-06 16:33:26 -0600

    Columnas y estilos

--- flutter_vscode/lib/src/pages/home_page.dart
+++ flutter_vscode/lib/src/pages/home_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  // TextStyle estiloTexto = new TextStyle(fontSize: 25); /* esto lo marca como inmutable, caracteristica de statelesswidget */
+  final estiloTexto = new TextStyle(
+      fontSize:
+          25); /* esto lo marca como inmutable, caracteristica de statelesswidget */
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,10 +13,21 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Título'),
           centerTitle: true,
-          elevation: 1.5,
         ),
         body: Center(
-          child: Text('Hola Mundo'),
-        ));
+            child: Column(
+          /* para poner varios elemenetos se necesita en columna */
+          mainAxisAlignment: MainAxisAlignment.center,
+          /* esto lo centran con el mainAxisAligment puedes centrarlo de varias maneras */
+          children: <Widget>[
+            Text(
+              'Número de taps:',
+              style: estiloTexto,
+            ),
+            Text('0', style: estiloTexto),
+          ],
+        )
+            /* no se puede poner otro child abajo, solo 1 */
+            ));
   }
 }
