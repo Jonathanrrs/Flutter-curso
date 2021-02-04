be400f9411ef201f388de826e62a88f97615078a
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-02-03 19:29:54 -0600

    Crear ListTiles a partir de una lista estática

--- components/lib/src/pages/home_temp.dart
+++ components/lib/src/pages/home_temp.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart'; /* imporm */
 
 class HomePageTemp extends StatelessWidget { /* stateless snipet */
- 
+  /* crear una propiedad con final porque se está dentro de un statelesswidget */
+
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
 
   @override
   Widget build(BuildContext context) {
@@ -10,21 +12,30 @@ class HomePageTemp extends StatelessWidget { /* stateless snipet */
         title: Text('Componentes Temp'),
       ),
       body: ListView( /* para crear listview */
-        children: <Widget>[
-          /* se puede mostrar en la lista cualquier widget */
-          ListTile( /* estos son un tipo de listas */
-            title: Text('ListTile Title'),
-          ),
-          Divider(), /* es para crear una linea espaceadora */
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('ListTile Title'),
-          )
-        ],
+       children: _crearItems()
       ), 
     );
   }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>(); /* retornar una lista de widgets */
+
+    for (String opt in opciones) {
+      
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista..add(tempWidget) /* cuando el ciclo termine se mostrará en el list */
+        ..add(Divider());
+      /* el .. es una forma en cascada para agregar otros elementos */
+
+      /* lista.add(Divider()); esta es otra forma */
+    }
+    return lista; /* retornamos la lista con los números */
+  }
+
+  // List<Widget> __crearItemsCorta() {
+
+  // }
+  
 }
