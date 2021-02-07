39bf5517c4a32331e2c9a9b63ce50687fe67bbb2
jonathan ramiro ruiz sandoval <yoni_12rrs@hotmail.com>
2021-02-06 21:44:34 -0600

    Página - HomePage

--- components/lib/main.dart
+++ components/lib/main.dart
@@ -1,7 +1,8 @@
 /* mateapp snipet para crear la interfaz necesario de una app */
+import 'package:components/src/pages/home_page.dart';
 import 'package:flutter/material.dart';
  
-import 'package:components/src/pages/home_temp.dart';
+// import 'package:components/src/pages/home_temp.dart';
 
 void main() => runApp(MyApp());
  
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false, /* quitar el debug incomodo de la app */
-      home: HomePageTemp(), /* se borró porque habian 2 scaffold */
+      home: HomePage(), /* se borró porque habian 2 scaffold */
     );
   }
 }
